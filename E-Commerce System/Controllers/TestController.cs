8b8bd2e3cfe827046363fbee2bc6cd71387311d5
Amin Ullah <Amin.Ullah@mremind.com>
2025-12-24 10:53:19 +0500

    Added TestController and Data folder

---
+++ E-Commerce System/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using E_Commerce_System.Data;
+
+namespace E_Commerce_System.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TestController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Example usage
+            return Ok("DbContext is working!");
+        }
+    }
+}
